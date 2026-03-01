81d10f2657b4c0b66b6922571ef3374ed4301085
Cielo Reksana Jaya <110617381+Cijay7@users.noreply.github.com>
2026-03-01 23:26:06 +0700

    Add EnigmaCipher class implementation

---
+++ ciphers/enigma.py
@@ -0,0 +1,20 @@
+class EnigmaCipher:
+    def __init__(self, rotors, reflector):
+        self.rotors = rotors
+        self.reflector = reflector
+
+    def encrypt(self, text):
+        # Implementation of encryption logic
+        pass
+
+    def decrypt(self, text):
+        # Implementation of decryption logic
+        pass
+
+    def set_rotors(self, positions):
+        # Logic to set the positions of the rotors
+        pass
+
+    def rotate_rotors(self):
+        # Logic to rotate the rotors
+        pass
